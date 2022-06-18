5b5512ce23315ec70ae11b6a587e0c1a9c6c270d
Aniket Nayek <nayekaniket2017@gmail.com>
2022-06-18 22:02:42 +0530

    Second Commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import LoadGen from './LoadGen';
 import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
 
 export class news extends Component {
+
+    static defaultProps = {
+        country: "in",
+        pageSize: 9,
+        category : "general"
+      }
+
+      static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+      }
     constructor(){
         super();
         this.state = {
@@ -15,7 +28,7 @@ export class news extends Component {
 
     async componentDidMount()
     {
-        let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=8d5238de0f324311bd264ca112742cf7&page=1&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d5238de0f324311bd264ca112742cf7&page=1&pageSize=${this.props.pageSize}`
         this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -26,7 +39,7 @@ export class news extends Component {
     }
 
      handleNext = async ()=>{
-        let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=8d5238de0f324311bd264ca112742cf7&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d5238de0f324311bd264ca112742cf7&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
         this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -40,7 +53,7 @@ export class news extends Component {
 
     handlePrev = async() =>{
         console.log("prev");
-        let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=8d5238de0f324311bd264ca112742cf7&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d5238de0f324311bd264ca112742cf7&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
         this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json();
